da5f61f6009b1436c500991f85fba6853b25fca7
Martin <escorpmartin97@gmail.com>
2023-06-27 23:03:27 -0300

    style: fix heigth img in skeletor component

--- tech-news-hub/src/app/components/loadingSkeletors/NewsEverything.tsx
+++ tech-news-hub/src/app/components/loadingSkeletors/NewsEverything.tsx
@@ -24,7 +24,7 @@ export default function LoadingNewsEverything() {
             <h2 className={`w-3/5 mb-3 ${skeletor}`}></h2>
             <p className={`${skeletor} w-1/3 mb-3`}></p>
 
-            <div className={`${skeletor} w-full rounded h-2/3`}></div>
+            <div className={`${skeletor} w-full rounded h-52`}></div>
 
             <p className={`${skeletor} mt-3 w-1/5`}></p>
           </div>
